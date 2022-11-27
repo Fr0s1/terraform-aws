8e645d5447ccf4e5079a91e9d566a8572c41daca
Bui Ngoc Thanh <thanhbn87@gmail.com>
2022-11-27 15:26:02 +0700

    Add group_name output

--- org/sso-group/outputs.tf
+++ org/sso-group/outputs.tf
@@ -3,6 +3,11 @@ output "group_id" {
   value       = try(aws_identitystore_group.this[0].group_id, "")
 }
 
+output "group_name" {
+  description = "The name of the created group"
+  value       = var.name
+}
+
 output "membership_ids" {
   description = "The membership ids"
   value       = try(aws_identitystore_group_membership.this, {})
